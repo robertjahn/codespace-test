00e15d414a16c85789b641913ee1ea915c7e4604
Rob Jahn <robjahn@gmail.com>
2026-01-12 16:17:34 -0500

    Replace environment variable access with os.getenv

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
+import os
+
 print("hello world")
-print("TEST_VAR1 = " + $TEST_VAR1)
-print("TEST_VAR2 = " + $TEST_VAR2)
-print("TEST_VAR3 = " + $TEST_VAR3)
+print("TEST_VAR1 = " + os.getenv("TEST_VAR1", ""))
+print("TEST_VAR2 = " + os.getenv("TEST_VAR2", ""))
+print("TEST_VAR3 = " + os.getenv("TEST_VAR3", ""))
