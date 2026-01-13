1b5316f76e5ec60eac2bb8187f2c405d4d8435ca
dynatrace-asad-ali <asad.ali@dynatrace.com>
2026-01-13 09:37:24 -0700

    change env variable names

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,8 +1,7 @@
-export DYNATRACE_EXPORTER_OTLP_ENDPOINT=$(curl -s -X POST http://52.186.168.229:3800/dynatrace-url \
+export DT_BASE_URL=$(curl -s -X POST http://52.186.168.229:3800/dynatrace-url \
   -H "Content-Type: application/json" \
-  -d "{\"password\": \"$WORKSHOP_PASSWORD\"}" | jq -r '.dynatrace_url')"/api/v2/otlp"
+  -d "{\"password\": \"$WORKSHOP_PASSWORD\"}" | jq -r '.dynatrace_url')
 
-echo $DYNATRACE_EXPORTER_OTLP_ENDPOINT
 export DYNATRACE_API_TOKEN=$(curl -s -X POST http://52.186.168.229:3800/dynatrace-token \
   -H "Content-Type: application/json" \
   -d "{\"password\": \"$WORKSHOP_PASSWORD\"}" | jq -r '.dynatrace_api_token')
@@ -11,6 +10,10 @@ export NVIDIA_API_KEY=$(curl -s -X POST http://52.186.168.229:3800/nvidia-key \
   -H "Content-Type: application/json" \
   -d "{\"password\": \"$WORKSHOP_PASSWORD\"}" | jq -r '.nvidia_api_key')
 
-export TAVILY_KEY=$(curl -s -X POST http://52.186.168.229:3800/tavily-key \
+export TAVILY_API_KEY=$(curl -s -X POST http://52.186.168.229:3800/tavily-key \
   -H "Content-Type: application/json" \
   -d "{\"password\": \"$WORKSHOP_PASSWORD\"}" | jq -r '.tavily_api_key')
+
+export OTEL_OLTP_ENDPOINT=$(curl -s -X POST http://52.186.168.229:3800/otel-endpoint \
+  -H "Content-Type: application/json" \
+  -d "{\"password\": \"$WORKSHOP_PASSWORD\"}" | jq -r '.otel_oltp_endpoint')
