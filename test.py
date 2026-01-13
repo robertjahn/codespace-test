bcab633040bb6d8e76d973e3e0e33fdf64b64bf1
Rob Jahn <robjahn@gmail.com>
2026-01-13 12:14:29 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import os
 
-print("hello world")
-print("TEST_VAR10 = " + os.getenv("TEST_VAR10", ""))
-print("TEST_VAR20 = " + os.getenv("TEST_VAR20", ""))
-print("TEST_VAR30 = " + os.getenv("TEST_VAR30", ""))
-print("TEST_VAR40 = " + os.getenv("TEST_VAR40", ""))
+print("DT_BASE_URL = " + os.getenv("DT_BASE_URL", ""))
+print("DYNATRACE_API_TOKEN = " + os.getenv("DYNATRACE_API_TOKEN", ""))
+print("NVIDIA_API_KEY = " + os.getenv("NVIDIA_API_KEY", ""))
+print("TAVILY_API_KEY = " + os.getenv("TAVILY_API_KEY", ""))
+print("OTEL_OTLP_ENDPOINT = " + os.getenv("OTEL_OTLP_ENDPOINT", ""))
